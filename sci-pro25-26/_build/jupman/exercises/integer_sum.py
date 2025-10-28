165c0c4721d09f85b4474470ecbb4611debe20e8
AntonioLonga <antonio.longa@gmail.com>
2025-10-28 16:06:58 +0100

    fix lab 10

--- sci-pro25-26/_build/jupman/exercises/integer_sum.py
+++
@@ -1,8 +0,0 @@
-""" integer_sum.py is a script to
- compute the sum of the first 1200 integers. """
-
-S = 0
-for i in range(0, 1201):
-    S = S + i
-
-print("The sum of the first 1200 integers is: ", S)
--- sci-pro25-26/exercises/square_example.py
+++
@@ -1,6 +0,0 @@
-"""
-square example
-"""
-S = int(input())
-AREA = S**2
-print("The area of square with size ", S, " is ", AREA)
